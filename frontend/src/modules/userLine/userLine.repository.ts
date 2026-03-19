6c6564eccce4d4016703059a13c24060f8d1e03b
kenkenman07 <ken0522g@icloud.com>
2026-03-20 03:26:46 +0900

    あ

--- frontend/src/modules/userLine/userLine.repository.ts
+++ frontend/src/modules/userLine/userLine.repository.ts
@@ -2,10 +2,15 @@ import { supabase } from "../../lib/supabase";
 
 export const userLineRepository = {
   async insert(userId: string, lineId: string) {
-    const { error } = await supabase.from("user_line").insert({
-      user_id: userId,
-      line_id: lineId,
-    });
+    const { error } = await supabase.from("user_line").upsert(
+      {
+        user_id: userId,
+        line_id: lineId,
+      },
+      {
+        onConflict: "user_id",
+      }
+    );
     if (error) throw new Error(error.message);
   },
 
